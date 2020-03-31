e50b712557ac38e156e431ebf65229032d300be0
look4yasharth <look4yasharth@gmail.com>
2020-03-31 07:22:20 +0530

    Colour Spin

---
+++ Project11 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,33 @@
+import Foundation
+import SpriteKit
+import WatchKit
+
+
+class InterfaceController: WKInterfaceController {
+    @IBOutlet var gameInterface: WKInterfaceSKScene!
+    var gameScene: GameScene!
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+
+        startGame(self)
+    }
+    
+    @IBAction func startGame(_ sender: Any) {
+        if gameScene != nil {
+            guard gameScene.isPlayerAlive == false else { return }
+        }
+
+        gameScene = GameScene(size: CGSize(width: 154, height: 174))
+        gameScene.parentInterfaceController = self
+        gameScene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+        let transition = SKTransition.doorway(withDuration: 1)
+        gameInterface.presentScene(gameScene, transition: transition)
+    }
+
+    override func didAppear() {
+        crownSequencer.focus()
+        crownSequencer.delegate = gameScene
+    }
+}
